618e19cd5e7de3a7e859f6597cf1acd01f7c4836
amwiti <developerbold@gmail.com>
2019-10-12 13:48:25 +0300

    Gender selection

---
+++ app/src/main/java/com/example/tinder/Registry/MatchyModel.kt
@@ -0,0 +1,5 @@
+package com.example.tinder.Registry
+
+class MatchyModel(val Name:String,val gender:String,val age:String,val location:String,val profilePic:String) {
+
+}
--- app/src/main/java/com/example/tinder/Registry/SignUpActivity.kt
+++ app/src/main/java/com/example/tinder/Registry/SignUpActivity.kt
@@ -6,10 +6,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.example.tinder.R
 import com.example.tinder.Settings.mainSettingsActivity
 import com.example.tinder.mainScreen
@@ -18,6 +15,7 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
@@ -28,6 +26,12 @@ class SignUpActivity : AppCompatActivity() {
     lateinit var email: String
     lateinit var password: String
     lateinit var password2: String
+    private lateinit var database: DatabaseReference
+
+    lateinit var radioGroup:RadioGroup
+    lateinit var  radioButton :RadioButton
+    lateinit var  userId:String
+    lateinit var Gender:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,28 +60,59 @@ class SignUpActivity : AppCompatActivity() {
         val e2: EditText = findViewById(R.id.passwordSignUp)
         val e3: EditText = findViewById(R.id.passwordSignUpConfirm)
 
+        radioGroup=dialog.findViewById(R.id.genderPopRadioGroup)
 
 
 
-        pdialog.setTitle("Signing you up")
+        //Getting Selected gender from the radioButton
+        radioGroup.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
+            override fun onCheckedChanged(p0: RadioGroup?, p1: Int) {
+
+                if(R.id.radioMale == p1){
+
+                    Gender = "Male"
+                }
+                else if (R.id.radioFemale == p1){
+                    Gender = "Female"
+                }
+            }
+        })
+
+
+      /*  pdialog.setTitle("Signing you up")
         pdialog.setMessage("wait a sec")
-        pdialog.setCanceledOnTouchOutside(false)
+        pdialog.setCanceledOnTouchOutside(false)*/
 
+        //getting Id of the current user
+        userId = fireAuth.getCurrentUser()!!.getUid()
 
         btn.setOnClickListener(View.OnClickListener {
             email = e1.text.toString()
             password = e2.text.toString()
+            dialog.show()
 
-            pdialog.show()
-            fireAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task: Task<AuthResult> ->
+            val text1:TextView=dialog.findViewById(R.id.submitText)
+
+            text1.setOnClickListener(View.OnClickListener {
+
+                Toast.makeText(applicationContext,""+Gender,Toast.LENGTH_LONG).show()
+            })
+
+
+           // pdialog.show()
+          /*  fireAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task: Task<AuthResult> ->
                 if (task.isSuccessful) {
 
                     dialog.show()
 
-                    val textt1:TextView=dialog.findViewById(R.id.submitText)
-                    textt1.setOnClickListener {
+                    val text1:TextView=dialog.findViewById(R.id.submitText)
 
-                    intent= Intent(this,mainSettingsActivity::class.java)
+                    text1.setOnClickListener {
+
+                        database = FirebaseDatabase.getInstance().reference.child("Users").child(radioButton.text.toString()).child(userId)
+
+
+                        intent= Intent(this,mainSettingsActivity::class.java)
                    startActivity(intent)
                    finish()
 
@@ -90,7 +125,7 @@ class SignUpActivity : AppCompatActivity() {
                     Toast.makeText(applicationContext,"Cant Register you",Toast.LENGTH_SHORT).show()
                 }
             }
-
+*/
 
         })
 
